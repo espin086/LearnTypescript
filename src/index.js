f77df4c93d9464f188935b196a4a7b48378cbccd
JJ <4843736+espin086@users.noreply.github.com>
2025-04-07 13:26:18 -0700

    running version of the code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
+//Variable Creations --------------------------------------------------------
 var menu = [
-    {
-        name: "Margherita",
-        price: 10
-    },
-    {
-        name: "Pepperoni",
-        price: 12
-    }
+    { name: "Margherita", price: 10 },
+    { name: "Pepperoni", price: 12 },
+    { name: "Veggie", price: 15 },
 ];
+var orderQueue = [];
+var cashInRegister = 0;
+var orderId = 0;
+//Functions ----------------------------------------------------------------
 function addPizza(pizza) {
     menu.push(pizza);
+    console.log("Pizza ".concat(pizza.name, " added to menu"));
 }
+function placeOrder(pizzaName) {
+    var pizza;
+    for (var i = 0; i < menu.length; i++) {
+        if (menu[i].name === pizzaName) {
+            pizza = menu[i];
+            break;
+        }
+    }
+    if (!pizza) {
+        console.log("Pizza ".concat(pizzaName, " not found in menu"));
+    }
+    else {
+        console.log("Order placed for ".concat(pizzaName));
+    }
+}
+function completeOrder(orderId) {
+    var order;
+    for (var i = 0; i < orderQueue.length; i++) {
+        if (orderQueue[i].id === orderId) {
+            order = orderQueue[i];
+            break;
+        }
+    }
+    if (!order) {
+        return "Order ".concat(orderId, " not found");
+    }
+    order.status = "complete";
+    return "Order ".concat(orderId, " is complete");
+}
+//Main Program ------------------------------------------------------------
 addPizza({ name: "Cheese", price: 15 });
+placeOrder("Cheese");
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,71 @@
+// Custom Types ------------------------------------------------------------
 type Pizza = {
     name: string
     price: number
 }
 
-const menu = [
-    {
-        name: "Margherita",
-        price: 10
-    },
-    {
-        name: "Pepperoni",
-        price: 12
-    }
-]
-
-
-function addPizza(pizza: Pizza): void {
-    menu.push(pizza)
+type Order = {
+    id: string
+    pizza: Pizza
+    status: string
 }
 
-addPizza({ name: "Cheese", price: 15 })
+type OrderQueue = {
+    orders: Order[]
+    cashInRegister: number
+    orderId: number
+}
 
+//Variable Creations --------------------------------------------------------
+const menu: Pizza[] = [
+    {name: "Margherita", price: 10},
+    {name: "Pepperoni", price: 12},
+    {name: "Veggie", price: 15},
+]
+
+let orderQueue: Order[] = []
+let cashInRegister: number = 0
+let orderId: number = 0
+
+//Functions ----------------------------------------------------------------
+function addPizza(pizza: Pizza): void {
+    menu.push(pizza)
+    console.log(`Pizza ${pizza.name} added to menu`)
+}
+
+
+function placeOrder(pizzaName: string): void {
+    let pizza: Pizza | undefined;
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i].name === pizzaName) {
+            pizza = menu[i];
+            break;
+        }
+    }
+    
+    if (!pizza) {
+        console.log(`Pizza ${pizzaName} not found in menu`)
+    } else {
+        console.log(`Order placed for ${pizzaName}`)
+    }
+}
+
+function completeOrder(orderId: string): string {
+    let order: Order | undefined;
+    for (let i = 0; i < orderQueue.length; i++) {
+        if (orderQueue[i].id === orderId) {
+            order = orderQueue[i];
+            break;
+        }
+    }
+    
+    if (!order) {
+        return `Order ${orderId} not found`;
+    }
+    order.status = "complete";
+    return `Order ${orderId} is complete`;
+}
+
+//Main Program ------------------------------------------------------------
+addPizza({ name: "Cheese", price: 15 })
+placeOrder("Cheese")
